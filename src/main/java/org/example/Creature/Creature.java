d3c36d9870a02496278b148bbd68dfe524ec5fc1
Sergey Starodubcev <starodubcev_2018@list.ru>
2025-01-25 20:00:51 +0200

    Changed in class Creature

--- src/main/java/org/example/Creature/Creature.java
+++ src/main/java/org/example/Creature/Creature.java
@@ -23,33 +23,31 @@ public abstract class Creature extends Entity {
     public Coordinates theShortestWay(List<Coordinates> listCoordinates, Creature creatureGoals, Coordinates creatureCoordinatesStep) {
         PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
         Map<Coordinates, Integer> coordinatesMap = new HashMap<>();
-        Coordinates shortestWay=creatureCoordinatesStep;
+        Coordinates shortestWay = creatureCoordinatesStep;
         List<Coordinates> lista = new ArrayList<>();
 
-            for (Coordinates s : listCoordinates) {
-                for (CoordinatesShift c : coordinatesShift) {
-                    if (shortestWay.x + c.xShift == s.x && shortestWay.y + c.yShift == s.y) {
-                        lista.add(s);
-                    }
+        for (Coordinates s : listCoordinates) {
+            for (CoordinatesShift c : coordinatesShift) {
+                if (shortestWay.x + c.xShift == s.x && shortestWay.y + c.yShift == s.y) {
+                    lista.add(s);
                 }
             }
+        }
+
+        for (Coordinates coordinates : lista) {
+            int sumCell = Math.abs(coordinates.x - creatureGoals.coordinates.x) + Math.abs(creatureGoals.coordinates.y - coordinates.y);
+            coordinatesMap.put(coordinates, sumCell);
+            priorityQueue.offer(sumCell);
+        }
 
 
-
-            for (Coordinates coordinates : lista) {
-                int sumCell =Math.abs (coordinates.x - creatureGoals.coordinates.x) + Math.abs(creatureGoals.coordinates.y - coordinates.y);
-                coordinatesMap.put(coordinates, sumCell);
-                priorityQueue.offer(sumCell);
+        for (Map.Entry<Coordinates, Integer> entry : coordinatesMap.entrySet()) {
+            int i = priorityQueue.poll();
+            if (entry.getValue() == i) {
+                shortestWay = entry.getKey();
+                break;
             }
-
-
-            for (Map.Entry<Coordinates, Integer> entry : coordinatesMap.entrySet()) {
-                int i = priorityQueue.poll();
-                if (entry.getValue() == i) {
-                    shortestWay = entry.getKey();
-                    break;
-                }
-            }
+        }
 
         return shortestWay;
     }
@@ -70,7 +68,6 @@ public abstract class Creature extends Entity {
         sets2.add(new Coordinates(0, 0));
 
         while (!queue.isEmpty()) {
-
 
             for (Coordinates coordinates2 : setser) {
                 if (!queue.contains(coordinates2)) {
@@ -94,7 +91,7 @@ public abstract class Creature extends Entity {
                 }
             }
 
-            creatureStart.coordinates=theShortestWay(setser, creatureGoals, creatureStart.coordinates);
+            creatureStart.coordinates = theShortestWay(setser, creatureGoals, creatureStart.coordinates);
             sets.add(creatureStart.coordinates);
 
         }
