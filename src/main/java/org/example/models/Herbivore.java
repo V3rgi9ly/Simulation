ffbb9c1bfe42b6bfacfea7f1b2c0b0bdef79cbeb
Sergey Starodubcev <starodubcev_2018@list.ru>
2025-02-17 00:04:03 +0200

    Changed LogicSimulation

--- src/main/java/org/example/models/Herbivore.java
+++ src/main/java/org/example/models/Herbivore.java
@@ -54,7 +54,6 @@ public class Herbivore extends Creature {
         }
     }
 
-
     private boolean isAdjacent(Coordinates target) {
         int dx = Math.abs(this.getCoordinates().x - target.x);
         int dy = Math.abs(this.getCoordinates().y - target.y);
